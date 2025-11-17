6c82d5104348e124ce3ab0272b83c25cd4d2b59e
Ph0enix-E <phoenixcedson@gmail.com>
2025-11-17 09:14:13 -0800

    a

--- SentinelSum.java
+++ SentinelSum.java
@@ -1,8 +1,18 @@
+import java.util.Scanner;
+
 public class SentinelSum {
    public static void main(String[] args) {
+      Scanner scan =  new Scanner(System.in);
       
+      System.out.print("Please enter a number to avergage in or -1 to stop: ");
+      int number = scan.nextInt();
+      int sum = 0;
       
-      
-      
+      while (number != -1) {
+         sum += number;
+         System.out.print("Please enter a number to avergage in or -1 to stop: ");
+         number = scan.nextInt();
+      }
+      System.out.println("Sum is " + sum);
    }
 }
